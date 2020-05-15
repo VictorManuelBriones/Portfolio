2195ac5c455d59ad5cdfc3746965843925a51f3b
Victor Manuel Briones <victorm140399@gmail.com>
2020-05-14 21:30:05 -0500

    Uniendo el Frontend con el Backend

--- core/views.py
+++ core/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
-html_base = """ 
-<h1>Mi web personal</h1>
-    <ul>
-        <li><a href="/">Portada</a></li>
-        <li><a href="/about-me/">Acerca de</a></li>
-        <li><a href="/portfolio/">Portafolio</a></li>
-        <li><a href="/contact/">Contacto</a></li>
 
-    </ul>
-
-"""
 # Create your views here.
 def home(request):
     return render(request, "core/home.html")
